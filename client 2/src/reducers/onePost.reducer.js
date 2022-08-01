673d8cedf1c671848ddca1ddfccdcfc1265c3a76
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-01 13:22:49 +0600

    user login, detailed post

--- client 2/src/reducers/onePost.reducer.js
+++
@@ -1,9 +0,0 @@
-export default function reduce  (onePost={}, action){
-  switch(action.type){
-    case 'FETCH_ONE' :
-      console.log("reduce fetch", action.payload)
-      return action.payload;
-    default:
-      return onePost;
-  }
-}
