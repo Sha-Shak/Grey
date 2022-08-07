9559c7431eb2cf659d0d693d9c1453ec48a32785
Mariana <marianapertuzv@gmail.com>
2022-08-07 16:52:37 -0500

    feat: navbar working with ts

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,42 +1,39 @@
 import { AppBar, Avatar, Button, Grid, Toolbar, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import logo from '../../Images/tlogo.png';
-import { styles } from './styles.ts';
+import { styles } from './styles';
 
 const Navbar = () => {
-  const storage = localStorage.getItem('user');
+  const storage: any = localStorage.getItem('user');
   const [user, setUser] = useState(JSON.parse(storage));
   const navigate = useNavigate();
-  
+
   useEffect(()=>{
     setUser(JSON.parse(storage));
   },[storage])
 
  
-  const handleLogOut = (user) => {
+  const handleLogOut = () => {
     try {
       localStorage.clear();
-      navigate('/auth')
+      setUser(null);
+      navigate('/auth');
     } catch(e) {
       console.log(e); 
     }
-    
-    user = null;
   }
 
   return (
      <AppBar sx={styles.appBar}  position="static" color="inherit">
       <Grid container sx={styles.brandContainer}>
-        <img style={styles.navBarImg} src={logo} alt="icon" height="60" />
+        <img style={styles.navBarImg} src={require('../../Images/tlogo.png')} alt="icon" height="60" />
         <Typography component = {Link} to="/" sx={styles.brandText} variant="h2" align="center">GREY</Typography>
       </Grid>
       <Toolbar sx={styles.toolbar}> 
         { user ? (
           <Grid container sx={styles.profile}>
             <Avatar sx={{marginRight: '10px'}} alt={user.result.name} >{user.result.name.charAt(0)}</Avatar>
-        {/* <Typography sx={{marginRight: '10px'}} variant="h6">{user.result.email.split(' ')[0]}</Typography> */}
-            <Button variant="contained" sx={{backgroundColor: '#dd0700'}} onClick={() => {handleLogOut(user)}}>Log Out</Button>
+            <Button variant="contained" style={{backgroundColor: '#dd0700'}} onClick={handleLogOut}>Log Out</Button>
           </Grid>
         ) : (
           <Button component={Link} to="/auth" variant="contained">Sign In</Button>
