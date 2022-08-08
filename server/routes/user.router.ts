f0094355e8927adc036863d44ffc202529da90b9
Mariana <marianapertuzv@gmail.com>
2022-08-08 18:04:24 -0500

    feat: frontend working with backend

--- server/routes/user.router.ts
+++ server/routes/user.router.ts
@@ -1,10 +1,8 @@
-import express from "express";
+import express from 'express';
 
 const { createUser, logInUser } = require('../controllers/users.controller.ts'); 
 const router = express.Router();
 
-
-//prefix added (localhost:8080/posts)
 router.post('/signin', logInUser)
 router.post('/signup', createUser)
 
