e3299dc446b3b0f0658db4e4104f8e00bd62e8a1
Mariana Pertuz <marianapertuzv@gmail.com>
2022-08-10 20:40:06 -0500

    Delete frontend/src/app directory

--- frontend/src/app/store.ts
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from '../reducers';
-
-export const store = createStore(reducers, applyMiddleware(thunk))
-// export const store = configureStore(reducers)
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
